b272791013d8e224ce95abd5e9f2f16f34f4f01b
jagadeesh-debug <jaggurockz4892@gmail.com>
2025-02-11 21:43:14 +0530

    done some modifications in package.json

--- Database/PostModel.js
+++ Database/PostModel.js
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
+    import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    user: { // Reference to the user who created the post
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',  // Reference to the User model
-    },
-});
+    const postSchema = new mongoose.Schema({
+        content: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        user: { // Reference to the user who created the post
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',  // Reference to the User model
+        },
+    });
 
-export default mongoose.models.Post || mongoose.model('Post', postSchema);
+    export default mongoose.models.Post || mongoose.model('Post', postSchema);
